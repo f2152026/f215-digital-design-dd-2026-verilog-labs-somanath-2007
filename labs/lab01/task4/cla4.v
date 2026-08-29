9a5171f0ab0da63551113a229a0727fb1a4e9c56
somanath-2007 <f20250717@pilani.bits-pilani.ac.in>
2026-08-29 11:36:50 +0000

    lab01 task4

--- labs/lab01/task4/cla4.v
+++ labs/lab01/task4/cla4.v
@@ -29,11 +29,45 @@ module cla4(
   output       cout
 );
 
-  wire p0, p1, p2, p3;
-  wire g0, g1, g2, g3;
-  wire c1, c2, c3;
 
-  // TODO: your gate-level P/G, carry, and sum logic goes here.
-  // (cout should be connected to c4.) Remember the delay on every gate.
+
+wire [3:0] p;
+wire [3:0] g;
+wire c1, c2, c3;
+wire k, l, m, n, v , q, r, s, w, y, d;
+
+
+xor #(2) (p[0], a[0], b[0]);
+xor #(2) (p[1], a[1], b[1]);
+xor #(2) (p[2], a[2], b[2]);
+xor #(2) (p[3], a[3], b[3]);
+
+and #(2) (g[0], a[0], b[0]);
+and #(2) (g[1], a[1], b[1]);
+and #(2) (g[2], a[2], b[2]);
+and #(2) (g[3], a[3], b[3]);
+
+
+and #(2) (k, p[0], cin);
+and #(2) (l, p[1], g[0]);
+and #(2) (m, p[2], g[1]);
+and #(2) (n, p[3], g[2]);
+and #(2) (v, p[0], cin, p[1]);
+and #(2) (q, p[1], g[0], p[2]);
+and #(2) (r, p[2], g[1], p[3]);
+and #(2) (s, p[2], p[1], p[0], cin);
+and #(2) (w, p[2], p[1], p[0], cin);
+and #(2) (y, p[1], g[0], p[2],p[3]);
+and #(2) (d, p[3], p[2], p[1], p[0], cin);
+or #(2) (c1, g[0], k);
+or #(2) (c2,g[1],l,v);
+or #(2) (c3,g[2],m,q,s);
+or #(2) (cout,g[3],n,r,y,d);
+
+
+ xor #(2) (sum[0],p[0],cin);
+ xor #(2) (sum[1],p[1],c1);
+ xor #(2) (sum[2],p[2],c2);
+ xor #(2) (sum[3],p[3],c3);
 
 endmodule
